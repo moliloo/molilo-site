f0e12214f7111107848f0565e9fe6eb5d00b9dc5
Murilo Brito <dev.murilobrito@gmail.com>
2026-02-26 19:42:12 -0300

    feat: finish project section

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import '$lib/styles/typography.css';
+    import '$lib/styles/glassy-container.css'
+    import { Magnifer } from '@solar-icons/svelte/Linear'
+    
+    export let text: string = '';
+</script>
+
+<div>
+</div>
+
+<div class="GlassContainer">
+  <div class="GlassContent">
+    <div class="search-input">
+        <input type="serach" class="primary-text" placeholder="search" bind:value={text}>
+        <Magnifer size={18}/>
+    </div>
+  </div>
+  <div class="GlassMaterial">
+    <div class="GlassEdgeReflection"></div>
+    <div class="GlassEmbossReflection"></div>
+    <div class="GlassRefraction"></div>
+    <div class="GlassBlur"></div>
+    <div class="BlendLayers"></div>
+    <div class="BlendEdge"></div>
+    <div class="Highlight"></div>
+  </div>
+</div>
+
+<style>
+    .GlassContainer {
+        position: sticky;
+        top: 0;
+        z-index: 999;
+    }
+
+    .search-input {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 20px;
+        color: var(--text);
+
+        input {
+            background: none;
+            outline: none;
+            border: none;
+        }
+    }
+</style>
---
+++ src/lib/sections/Projects.svelte
@@ -0,0 +1,151 @@
+<script lang='ts'>
+    import '$lib/styles/glassy-container.css'
+    import '$lib/styles/typography.css'
+    import LineHeader from '$lib/components/LineHeader.svelte';
+    import SearchBar from '$lib/components/SearchBar.svelte';
+    import ProjectCard from '$lib/components/ProjectCard.svelte';
+    import '@splidejs/svelte-splide/css/core';
+
+    let searchText: string = '';
+
+    $: filteredProjects = projects.filter(project =>
+        project.title.toLowerCase().includes(searchText.toLowerCase()) ||
+        project.tags.some(tag =>
+            tag.toLowerCase().includes(searchText.toLowerCase())
+        )
+    );
+
+    const stackUp = [
+        'sites',
+        'ui/ux',
+    ];
+
+    const stackDown = [
+        '3d',
+        'foundryvtt projects'
+    ];
+
+    let projects = [
+        {
+            title: 'Lorem Ispum Pudim',
+            img: 'https://images-ext-1.discordapp.net/external/Pd1GbfH6m13Z6BAqsqos47NfkpvFZHtYIkyq7JFfngE/https/pbs.twimg.com/media/HCDst7BbUAASsiP.jpg%3Alarge?format=webp&width=630&height=945',
+            tags: [
+                'tag 1',
+                'tag 2',
+                'tag 3',
+                'tag 4',
+            ]
+        },
+        {
+            title: 'Lorem Ispum Amassado',
+            img: 'https://images-ext-1.discordapp.net/external/Pd1GbfH6m13Z6BAqsqos47NfkpvFZHtYIkyq7JFfngE/https/pbs.twimg.com/media/HCDst7BbUAASsiP.jpg%3Alarge?format=webp&width=630&height=945',
+            tags: [
+                'tag 1',
+                'tag 2',
+                'tag 3',
+                'tag 4',
+            ]
+        },
+        {
+            title: 'Lorem Ispum',
+            img: 'https://images-ext-1.discordapp.net/external/Pd1GbfH6m13Z6BAqsqos47NfkpvFZHtYIkyq7JFfngE/https/pbs.twimg.com/media/HCDst7BbUAASsiP.jpg%3Alarge?format=webp&width=630&height=945',
+            tags: [
+                'tag 1',
+                'tag 2',
+                'tag 3',
+                'tag 4',
+            ]
+        },
+        {
+            title: 'Lorem Ispum',
+            img: 'https://images-ext-1.discordapp.net/external/Pd1GbfH6m13Z6BAqsqos47NfkpvFZHtYIkyq7JFfngE/https/pbs.twimg.com/media/HCDst7BbUAASsiP.jpg%3Alarge?format=webp&width=630&height=945',
+            tags: [
+                'tag 1',
+                'tag 2',
+                'tag 3',
+                'tag 4',
+            ]
+        },
+        {
+            title: 'Lorem Ispum',
+            img: 'https://images-ext-1.discordapp.net/external/Pd1GbfH6m13Z6BAqsqos47NfkpvFZHtYIkyq7JFfngE/https/pbs.twimg.com/media/HCDst7BbUAASsiP.jpg%3Alarge?format=webp&width=630&height=945',
+            tags: [
+                'tag 1',
+                'tag 2',
+                'tag 3',
+                'tag 4',
+            ]
+        },
+        {
+            title: 'Lorem Ispum',
+            img: 'https://images-ext-1.discordapp.net/external/Pd1GbfH6m13Z6BAqsqos47NfkpvFZHtYIkyq7JFfngE/https/pbs.twimg.com/media/HCDst7BbUAASsiP.jpg%3Alarge?format=webp&width=630&height=945',
+            tags: [
+                'tag 1',
+                'tag 2',
+                'tag 3',
+                'tag 4',
+            ]
+        },
+    ]
+</script>
+
+<div class="projects">
+    <h1 class="section-title section-header left-section">projects</h1>
+    <div class="project-section">
+        <LineHeader labelUp={stackUp} labelDown={stackDown} />
+
+        <div class="project-inner">
+            <SearchBar bind:text={searchText} />
+
+            <div class="project-list">
+                {#each filteredProjects as project}
+                    <ProjectCard project={project} />
+                {/each}
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .projects {
+        display: flex;
+        align-items: flex-start;
+        gap: 36px;
+        width: calc(100dvw - 80px);
+        height: 75%;
+        margin-top: 40px;
+    }
+    .left-section {
+        flex: 0 0 18%;
+        margin-left: 5%;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .project-section {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        height: 100%;
+        width: 62%;
+
+        .project-inner {
+            flex: 1 1 auto;
+            height: 90%;
+            overflow-y: auto;
+            scroll-snap-type: y mandatory;
+            -webkit-overflow-scrolling: touch;
+            overflow-x: hidden;
+
+            .project-list {
+                display: flex;
+                flex-wrap: wrap;
+                width: 100%;
+                gap: 40px;
+                justify-content: space-between;
+                margin-top: 40px;
+            }
+        }
+    }
+</style>
